e6ee7cb5f9a451e4a1d77b92e9bf8fb96289eaae
Denis Buserski <denis.buserski@gmail.com>
2023-11-17 10:33:45 +0200

    Update display-pictures.js

--- display-pictures.js
+++ display-pictures.js
@@ -30,8 +30,7 @@ function loadImages() {
 
 async function fetchImageFiles(folderPath) {
   try {
-    // const apiUrl = `https://api.github.com/repos/denisbuserski/photography-db/contents${folderPath}`;
-    const apiUrl = `https://api.github.com/repos/denisbuserski/photography-db/contents/photography-db/${folderPath}`;
+    const apiUrl = `https://api.github.com/repos/denisbuserski/photography-db/contents${folderPath}`;
     const response = await fetch(apiUrl);
     
     if (!response.ok) {
